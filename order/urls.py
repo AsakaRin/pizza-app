70c1788efc148089fa656d756c0f0d99ccea1fd5
NGOCANH198 <88376657+NGOCANH198@users.noreply.github.com>
2021-11-20 23:44:02 +0700

    add

--- order/urls.py
+++ order/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.home, name="home"),
+	path("", views.home, name="home"), 
 	path("<str:option>/<int:item_id>", views.custom, name="custom"),
 	path("cart", views.cart, name="cart"),
 	path("mycart", views.mycart, name="mycart"),
